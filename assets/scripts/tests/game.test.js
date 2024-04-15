15967daf67a63ff0668d2ae34dd420fa0b4f19ee
Tom Widdows <thomas.widdows10@gmail.com>
2024-04-15 18:46:21 +0000

    Add newGame() function. add newGame test.

--- assets/scripts/tests/game.test.js
+++ assets/scripts/tests/game.test.js
@@ -29,3 +29,13 @@ describe("game object contains correct keys", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+});
